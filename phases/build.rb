3c1ae9644d640f9757b8a2fc2bd5a1719628105b
takumin <takumiiinn@gmail.com>
2020-03-25 15:55:41 +0900

    move debootstrap recipe to initialize phase

--- phases/build.rb
+++ phases/build.rb
@@ -1,4 +1,3 @@
 include_recipe File.expand_path('../../cookbooks/validate', __FILE__)
-include_recipe File.expand_path('../../cookbooks/debootstrap', __FILE__)
 include_recipe File.expand_path('../../cookbooks/apt', __FILE__)
 include_recipe File.expand_path('../../cookbooks/hosts', __FILE__)
--- phases/initialize.rb
+++ phases/initialize.rb
@@ -1,3 +1,4 @@
 include_recipe File.expand_path('../../cookbooks/validate', __FILE__)
 include_recipe File.expand_path('../../cookbooks/messages', __FILE__)
 include_recipe File.expand_path('../../cookbooks/packages', __FILE__)
+include_recipe File.expand_path('../../cookbooks/debootstrap', __FILE__)
