5dcc6ff872ab208f323acf75d7a397da7e01d667
takumin <takumiiinn@gmail.com>
2020-03-28 04:14:59 +0900

    fix rootfs_archive cookbook

--- cookbooks/rootfs_archive/default.rb
+++ cookbooks/rootfs_archive/default.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 #
 # Public Variables
 #
 
-node[:finalize]              ||= Hashie::Mash.new
-node[:finalize][:target_dir] ||= ENV['TARGET_DIRECTORY'] || node[:target][:directory]
-node[:finalize][:output_dir] ||= ENV['OUTPUT_DIRECTORY'] || File.join(
+node[:rootfs_archive]              ||= Hashie::Mash.new
+node[:rootfs_archive][:target_dir] ||= ENV['TARGET_DIRECTORY'] || node[:target][:directory]
+node[:rootfs_archive][:output_dir] ||= ENV['OUTPUT_DIRECTORY'] || File.join(
   'releases',
   node[:target][:distribution],
   node[:target][:suite],
@@ -16,8 +18,8 @@ node[:finalize][:output_dir] ||= ENV['OUTPUT_DIRECTORY'] || File.join(
 # Private Variables
 #
 
-target_dir = node[:finalize][:target_dir]
-output_dir = node[:finalize][:output_dir]
+target_dir = node[:rootfs_archive][:target_dir]
+output_dir = node[:rootfs_archive][:output_dir]
 
 #
 # Required Packages
@@ -48,27 +50,28 @@ end
 # SquashFS Archive
 #
 
-execute "mksquashfs #{target_dir} #{output_dir}/rootfs.squashfs -comp xz" do
-  not_if "test -f #{output_dir}/rootfs.squashfs"
+if ENV['DISABLE_SQUASHFS'] != 'true'
+  execute "mksquashfs #{target_dir} #{output_dir}/rootfs.squashfs -comp xz" do
+    not_if "test -f #{output_dir}/rootfs.squashfs"
+  end
+
+  file "#{output_dir}/rootfs.squashfs" do
+    owner 'root'
+    group 'root'
+    mode  '0644'
+  end
 end
 
 #
 # Tarball Archive
 #
 
-execute "tar -I pixz -p --acls --xattrs --one-file-system -cf #{output_dir}/rootfs.tar.xz -C #{target_dir} ." do
-  not_if "test -f #{output_dir}/rootfs.tar.xz"
-end
+if ENV['DISABLE_TARBALL'] != 'true'
+  execute "tar -I pixz -p --acls --xattrs --one-file-system -cf #{output_dir}/rootfs.tar.xz -C #{target_dir} ." do
+    not_if "test -f #{output_dir}/rootfs.tar.xz"
+  end
 
-#
-# File Permission
-#
-
-%W{
-  #{output_dir}/rootfs.squashfs
-  #{output_dir}/rootfs.tar.xz
-}.each do |archive|
-  file archive do
+  file "#{output_dir}/rootfs.tar.xz" do
     owner 'root'
     group 'root'
     mode  '0644'
