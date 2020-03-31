bb713574c8facaa5f117da5c5bfd13b19708d82a
takumin <takumiiinn@gmail.com>
2020-03-31 19:02:05 +0900

    update cleanup cookbook

--- cookbooks/cleanup/default.rb
+++ cookbooks/cleanup/default.rb
@@ -13,10 +13,25 @@ file '/etc/machine-id' do
   owner   'root'
   group   'root'
   mode    '0644'
-  content ''
 end
 
 link '/var/lib/dbus/machine-id' do
   to '/etc/machine-id'
   force true
+end
+
+directory '/var/lib/apt/lists' do
+  action :delete
+end
+
+directory '/var/lib/apt/lists' do
+  owner 'root'
+  group 'root'
+  mode  '0755'
+end
+
+file '/var/lib/apt/lists/lock' do
+  owner 'root'
+  group 'root'
+  mode  '0640'
 end
