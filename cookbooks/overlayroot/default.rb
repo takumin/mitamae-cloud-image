8770823b16e5badfa4d0e2e3f690a28429753995
takumin <takumiiinn@gmail.com>
2020-04-27 14:03:44 +0900

    add overlayroot cookbook

---
+++ cookbooks/overlayroot/default.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+#
+# Package Install
+#
+
+package 'cloud-initramfs-copymods'
+package 'cloud-initramfs-dyn-netconf'
+package 'cloud-initramfs-rooturl'
+package 'overlayroot'
