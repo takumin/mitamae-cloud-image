0e074fef868dbe5a1b454780dfa8c132feb071ed
takumin <takumiiinn@gmail.com>
2020-11-17 04:05:29 +0900

    enable and update wireguard cookbook

--- cookbooks/wireguard/default.rb
+++ cookbooks/wireguard/default.rb
@@ -1,33 +1,6 @@
 # frozen_string_literal: true
 
 #
-# Apt Repository
-#
-
-apt_keyring 'Launchpad PPA for wireguard-ppa' do
-  finger 'E1B39B6EF6DDB96564797591AE33835F504A1A25'
-end
-
-apt_repository 'WireGuard Repository' do
-  path '/etc/apt/sources.list.d/wireguard.list'
-  entry [
-    {
-      :default_uri => 'http://ppa.launchpad.net/wireguard/wireguard/ubuntu',
-      :mirror_uri  => "#{ENV['APT_REPO_URL_PPA_WIREGUARD']}",
-      :suite       => '###platform_codename###',
-      :components  => [
-        'main',
-      ],
-    },
-  ]
-  notifies :run, 'execute[apt-get update]', :immediately
-end
-
-execute 'apt-get update' do
-  action :nothing
-end
-
-#
 # Required Packages
 #
 
@@ -38,6 +11,8 @@ when /^ubuntu-16\.04-generic-hwe$/
   package 'linux-headers-generic-hwe-16.04'
 when /^ubuntu-18\.04-generic-hwe$/
   package 'linux-headers-generic-hwe-18.04'
+when /^ubuntu-20\.04-generic-hwe$/
+  package 'linux-headers-generic-hwe-20.04'
 else
   MItamae.logger.error "Unknown platform: #{node[:platform]}-#{node[:platform_version]}-#{node[:target][:kernel]}"
   exit 1
