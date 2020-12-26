aad7b2f56c387ff2fe55d0ad0cf561319e0fbce8
takumin <takumiiinn@gmail.com>
2020-12-27 04:28:36 +0900

    change liveboot package

--- cookbooks/liveboot/default.rb
+++ cookbooks/liveboot/default.rb
@@ -5,12 +5,7 @@
 #
 
 case node[:platform]
-when 'ubuntu'
-  package 'cloud-initramfs-dyn-netconf'
-  package 'cloud-initramfs-copymods'
-  package 'cloud-initramfs-rooturl'
-  package 'overlayroot'
-when 'debian'
+when 'debian', 'ubuntu'
   package 'live-boot'
 else
   MItamae.logger.error "Unknown Platform: #{node[:platform]}"
