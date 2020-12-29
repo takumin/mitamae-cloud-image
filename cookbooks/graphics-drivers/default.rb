05fc66297f130ed427556c33f3d5c45612082630
takumin <takumiiinn@gmail.com>
2020-12-29 11:07:05 +0900

    update graphics-drivers recipe

--- cookbooks/graphics-drivers/default.rb
+++ cookbooks/graphics-drivers/default.rb
@@ -1,100 +1,81 @@
 # frozen_string_literal: true
 
 #
-# Check Platform
+# Check Role
 #
-
-unless node[:platform].match(/^ubuntu$/)
-  return
-end
 
 unless node[:target][:role].match(/nvidia/)
   return
 end
 
 #
-# Constant Variables
+# Apt Repository
 #
 
-NVIDIA_DRIVER_VERSION_AVAILABLE = [
-  '390', # Old LTS
-  '450', # Latest Long Lived Branch
-  # '455', # Latest Short Lived Branch (Unknown Server Package...)
-]
+if node[:platform] == 'ubuntu'
+  apt_keyring 'Launchpad PPA for Graphics Drivers Team' do
+    finger '2388FF3BE10A76F638F80723FCAE110B1118213C'
+  end
 
-#
-# Public Variables
-#
+  apt_repository 'Graphics Drivers Repository' do
+    path '/etc/apt/sources.list.d/graphics-drivers.list'
+    entry [
+      {
+        :default_uri => 'http://ppa.launchpad.net/graphics-drivers/ppa/ubuntu',
+        :mirror_uri  => "#{ENV['APT_REPO_URL_PPA_GRAPHICS_DRIVERS']}",
+        :suite       => '###platform_codename###',
+        :components  => [
+          'main',
+        ],
+      },
+    ]
+    notifies :run, 'execute[apt-get update]', :immediately
+  end
 
-node[:graphics_drivers]                  ||= Hashie::Mash.new
-node[:graphics_drivers][:nvidia_version] ||= NVIDIA_DRIVER_VERSION_AVAILABLE.last
-
-#
-# Validate Variables
-#
-
-node.validate! do
-  {
-    graphics_drivers: {
-      nvidia_version: match(/^(?:#{NVIDIA_DRIVER_VERSION_AVAILABLE.join('|')})$/),
-    },
-  }
+  execute 'apt-get update' do
+    action :nothing
+  end
 end
 
 #
 # Required Packages
 #
 
-if node[:platform_version].match(/(?:16\.04|18\.04)/)
-  if node[:target][:kernel].match(/hwe/)
-    if node[:target][:role].match(/server/)
-      package "xserver-xorg-core-hwe-#{node[:platform_version]}"
-      package "xserver-xorg-input-all-hwe-#{node[:platform_version]}"
-      package "xserver-xorg-legacy-hwe-#{node[:platform_version]}"
-    end
-
-    if node[:target][:role].match(/desktop/)
-      package "xserver-xorg-hwe-#{node[:platform_version]}"
-    end
-  end
-end
-
-#
-# Apt Repository
-#
-
-apt_keyring 'Launchpad PPA for Graphics Drivers Team' do
-  finger '2388FF3BE10A76F638F80723FCAE110B1118213C'
-end
-
-apt_repository 'Graphics Drivers Repository' do
-  path '/etc/apt/sources.list.d/graphics-drivers.list'
-  entry [
-    {
-      :default_uri => 'http://ppa.launchpad.net/graphics-drivers/ppa/ubuntu',
-      :mirror_uri  => "#{ENV['APT_REPO_URL_PPA_GRAPHICS_DRIVERS']}",
-      :suite       => '###platform_codename###',
-      :components  => [
-        'main',
-      ],
-    },
-  ]
-  notifies :run, 'execute[apt-get update]', :immediately
-end
-
-execute 'apt-get update' do
-  action :nothing
+case "#{node[:platform]}-#{node[:platform_version]}-#{node[:target][:kernel]}-#{node[:target][:role]}"
+when /^ubuntu-(?:16|18|20)\.04-generic-(?:desktop|server)-nvidia$/
+  # nothing...
+when 'ubuntu-16.04-generic-hwe-desktop-nvidia'
+  package 'xserver-xorg-hwe-16.04'
+when 'ubuntu-16.04-generic-hwe-server-nvidia'
+  package 'xserver-xorg-legacy-hwe-16.04'
+when 'ubuntu-18.04-generic-hwe-desktop-nvidia'
+  package 'linux-headers-generic-hwe-18.04'
+  package 'xserver-xorg-hwe-18.04'
+when 'ubuntu-18.04-generic-hwe-server-nvidia'
+  package 'linux-headers-generic-hwe-18.04'
+when /^ubuntu-20\.04-generic-hwe-(?:desktop|server)-nvidia$/
+  package 'linux-headers-generic-hwe-20.04'
+else
+  MItamae.logger.error "#{node[:platform]}-#{node[:platform_version]}-#{node[:target][:kernel]}-#{node[:target][:role]}"
+  exit 1
 end
 
 #
 # Install Package
 #
 
-case node[:target][:role]
-when /server/
-  package "nvidia-driver-#{node[:graphics_drivers][:nvidia_version]}-server"
-when /desktop/
-  package "nvidia-driver-#{node[:graphics_drivers][:nvidia_version]}"
+case "#{node[:platform]}-#{node[:platform_version]}-#{node[:target][:role]}"
+when 'ubuntu-16.04-desktop-nvidia'
+  package 'nvidia-430'
+when 'ubuntu-16.04-server-nvidia'
+  package 'nvidia-430' do
+    options '--no-install-recommends'
+  end
+when /^ubuntu-(?:18\.04|20\.04)-desktop-nvidia$/
+  package 'nvidia-driver-450'
+when /^ubuntu-(?:18\.04|20\.04)-server-nvidia$/
+  package 'nvidia-headless-450-server'
 else
-  raise
+  MItamae.logger.error "#{node[:platform]}-#{node[:platform_version]}-#{node[:target][:role]}"
+  exit 1
 end
