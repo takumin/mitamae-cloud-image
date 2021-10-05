dd2ec59f197d7bf0e84070e535294d75eb702c78
takumin <takumiiinn@gmail.com>
2021-10-05 19:17:40 +0900

    fix arch linux wireguard packages

--- cookbooks/wireguard/default.rb
+++ cookbooks/wireguard/default.rb
@@ -8,11 +8,6 @@ case node[:platform]
 when 'debian', 'ubuntu'
   package 'wireguard'
 when 'arch'
-  if node[:target][:kernel].match(/lts/)
-    package 'wireguard-lts'
-  else
-    package 'wireguard-dkms'
-  end
   package 'wireguard-tools'
 else
   raise
