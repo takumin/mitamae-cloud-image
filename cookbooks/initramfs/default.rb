47f2da64ad102aaadacfa42b4878fe3862fbdd9a
takumin <takumiiinn@gmail.com>
2021-10-12 01:50:42 +0900

    support initramfs compress format

--- cookbooks/initramfs/default.rb
+++ cookbooks/initramfs/default.rb
@@ -1,12 +1,41 @@
 # frozen_string_literal: true
 
 #
+# Public Variables
+#
+
+node[:initramfs]            ||= Hashie::Mash.new
+node[:initramfs][:compress] ||= 'xz'
+
+#
 # Package Install
 #
 
 case node[:platform]
 when 'debian', 'ubuntu'
   package 'initramfs-tools'
+
+  case node[:initramfs][:compress]
+  when 'gzip'
+    package 'pigz'
+  when 'lz4'
+    package 'liblz4-tool'
+  when 'xz'
+    package 'pixz'
+  when 'zstd'
+    package 'zstd'
+  else
+    raise
+  end
+
+  file '/etc/initramfs-tools/initramfs.conf' do
+    action :edit
+    block do |content|
+      unless content.match(/^COMPRESS=#{node[:initramfs][:compress]}$/)
+        content.gsub!(/^COMPRESS=.*/, "COMPRESS=#{node[:initramfs][:compress]}")
+      end
+    end
+  end
 when 'arch'
   package 'mkinitcpio'
 else
