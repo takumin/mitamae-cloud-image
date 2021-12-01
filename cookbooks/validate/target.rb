8b8a65b77aac3d7965f784c6eee08b2e2f655a89
takumin <takumiiinn@gmail.com>
2021-12-02 01:53:14 +0900

    fix target node inventory validation

--- cookbooks/validate/target.rb
+++ cookbooks/validate/target.rb
@@ -2,12 +2,22 @@
 # Validate Variables
 #
 
+AVAILABLE_ROLES = [
+  'minimal',
+  'server',
+  'desktop',
+  'server-nvidia',
+  'desktop-nvidia',
+  'server-nvidia-cuda',
+  'desktop-nvidia-cuda',
+]
+
 node.validate! do
   {
     target: {
       distribution: match(/^(?:debian|ubuntu|arch)$/),
       architecture: match(/^(?:i386|amd64|armhf|arm64)$/),
-      role:         match(/^(?:minimal|server|desktop|server-nvidia|desktop-nvidia)$/),
+      role:         match(/^(?:#{AVAILABLE_ROLES.join('|')})$/),
       directory:    string,
     },
   }
@@ -19,7 +29,7 @@ when :ubuntu
     {
       target: {
         kernel:     match(/^(?:generic|generic-hwe|virtual|virtual-hwe)$/),
-        suite:      match(/^(?:xenial|bionic)$/),
+        suite:      match(/^(?:xenial|bionic|focal)$/),
         components: array_of(match(/^(?:main|restricted|universe|multiverse)$/)),
       },
     }
@@ -29,7 +39,7 @@ when :debian
     {
       target: {
         kernel:     match(/^(?:generic|cloud)$/),
-        suite:      match(/^(?:jessie|stretch|buster)$/),
+        suite:      match(/^(?:jessie|stretch|buster|bullseye)$/),
         components: array_of(match(/^(?:main|contrib|non-free)$/)),
       },
     }
