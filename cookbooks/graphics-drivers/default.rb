0cebea756c2ea2d5b16dff0b94a8bb0e04b1d1dc
takumin <takumiiinn@gmail.com>
2021-12-03 00:33:23 +0900

    Revert "fix graphics-drivers recipe"
    This reverts commit 89d6617fbcd3090663e5e4fb32ab36700344cf2c.

--- cookbooks/graphics-drivers/default.rb
+++ cookbooks/graphics-drivers/default.rb
@@ -44,20 +44,16 @@ end
 case "#{node[:platform]}-#{node[:platform_version]}-#{node[:target][:kernel]}"
 when /^ubuntu-(?:16|18|20)\.04-generic-(?:desktop|server)$/
   # nothing...
-when 'ubuntu-16.04-generic-hwe'
-  if node[:target][:roles].any?{|v| v.match?(/desktop/)}
-    package 'xserver-xorg-hwe-16.04'
-  else
-    package 'xserver-xorg-legacy-hwe-16.04'
-  end
-when 'ubuntu-18.04-generic-hwe'
-  if node[:target][:roles].any?{|v| v.match?(/desktop/)}
-    package 'linux-headers-generic-hwe-18.04'
-    package 'xserver-xorg-hwe-18.04'
-  else
-    package 'linux-headers-generic-hwe-18.04'
-  end
-when /^ubuntu-20\.04-generic-hwe$/
+when 'ubuntu-16.04-generic-hwe-desktop'
+  package 'xserver-xorg-hwe-16.04'
+when 'ubuntu-16.04-generic-hwe-server'
+  package 'xserver-xorg-legacy-hwe-16.04'
+when 'ubuntu-18.04-generic-hwe-desktop'
+  package 'linux-headers-generic-hwe-18.04'
+  package 'xserver-xorg-hwe-18.04'
+when 'ubuntu-18.04-generic-hwe-server'
+  package 'linux-headers-generic-hwe-18.04'
+when /^ubuntu-20\.04-generic-hwe-(?:desktop|server)$/
   package 'linux-headers-generic-hwe-20.04'
 else
   MItamae.logger.error "#{node[:platform]}-#{node[:platform_version]}-#{node[:target][:kernel]}"
@@ -69,20 +65,16 @@ end
 #
 
 case "#{node[:platform]}-#{node[:platform_version]}"
-when 'ubuntu-16.04'
-  if node[:target][:roles].any?{|v| v.match?(/desktop/)}
-    package 'nvidia-430'
-  else
-    package 'nvidia-430' do
-      options '--no-install-recommends'
-    end
+when 'ubuntu-16.04-desktop'
+  package 'nvidia-430'
+when 'ubuntu-16.04-server'
+  package 'nvidia-430' do
+    options '--no-install-recommends'
   end
-when /^ubuntu-(?:18\.04|20\.04)$/
-  if node[:target][:roles].any?{|v| v.match?(/desktop/)}
-    package 'nvidia-driver-470'
-  else
-    package 'nvidia-headless-470-server'
-  end
+when /^ubuntu-(?:18\.04|20\.04)-desktop$/
+  package 'nvidia-driver-470'
+when /^ubuntu-(?:18\.04|20\.04)-server$/
+  package 'nvidia-headless-470-server'
 else
   MItamae.logger.error "#{node[:platform]}-#{node[:platform_version]}"
   exit 1
