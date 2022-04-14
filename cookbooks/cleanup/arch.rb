075f7af0c2a9264b05e04dda199f3c7747b141cf
takumin <takumiiinn@gmail.com>
2022-04-14 14:22:59 +0900

    re-generate initramfs image for arch linux

--- cookbooks/cleanup/arch.rb
+++ cookbooks/cleanup/arch.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
 #
+# Create Initramfs
+#
+
+execute 'mkinitcpio -g /boot/initramfs-linux.img -k /boot/vmlinuz-linux -c /etc/mkinitcpio.conf -S autodetect' do
+  only_if 'test -f /boot/initramfs-linux-fallback.img'
+end
+
+#
+# Remove Initramfs Fallback
+#
+
+file '/boot/initramfs-linux-fallback.img' do
+  action :delete
+end
+
+#
 # Upgrade Packages
 #
 
