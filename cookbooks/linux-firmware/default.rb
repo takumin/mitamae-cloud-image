17ab35fb47fa6bf4a6df69d07f276d5d5178755e
takumin <takumiiinn@gmail.com>
2022-05-06 04:31:14 +0900

    cookbooks/linux-firmware: add recipe

---
+++ cookbooks/linux-firmware/default.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+#
+# Public Variables
+#
+
+node.reverse_merge!({
+  linux_firmware: {
+    excludes: [],
+    includes: [],
+  },
+})
+
+#
+# Raspberry Pi
+#
+
+if node.target.kernel == 'raspi'
+  node.linux_firmware.excludes << '/lib/firmware/*'
+  node.linux_firmware.includes << '/lib/firmware/brcm/*'
+  node.linux_firmware.includes << '/lib/firmware/cypress/*'
+end
+
+#
+# Validate Variables
+#
+
+node.validate! do
+  {
+    linux_firmware: {
+      excludes: array_of(string),
+      includes: array_of(string),
+    },
+  }
+end
+
+#
+# Check Empty Variables
+#
+
+if node.linux_firmware.excludes.empty? and node.linux_firmware.includes.empty?
+  return
+end
+
+#
+# [Ex|In]cludes Firmware
+#
+
+case node.platform
+when 'debian', 'ubuntu'
+  contents = []
+
+  node.linux_firmware.excludes.each do |path|
+    contents << "path-exclude #{path}"
+  end
+
+  node.linux_firmware.includes.each do |path|
+    contents << "path-include #{path}"
+  end
+
+  file '/etc/dpkg/dpkg.cfg.d/linux-firmware' do
+    owner   'root'
+    group   'root'
+    mode    '0644'
+    content "#{contents.join("\n")}\n"
+    notifies :run, 'execute[apt-get install -y --reinstall linux-firmware]', :immediately
+  end
+
+  execute 'apt-get install -y --reinstall linux-firmware' do
+    action :nothing
+  end
+end
