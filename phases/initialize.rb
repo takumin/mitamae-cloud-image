79101e9115b5aa1711882bb06dcf803330cfece8
takumin <takumiiinn@gmail.com>
2022-05-05 16:08:08 +0900

    rename cookbooks/messages to helpers/messages

--- phases/initialize.rb
+++ phases/initialize.rb
@@ -2,10 +2,10 @@
 # Helpers
 #
 include_recipe File.expand_path('../../helpers/validate', __FILE__)
+include_recipe File.expand_path('../../helpers/messages', __FILE__)
 #
 # Recipes
 #
-include_recipe File.expand_path('../../cookbooks/messages', __FILE__)
 include_recipe File.expand_path('../../cookbooks/debootstrap', __FILE__)
 include_recipe File.expand_path('../../cookbooks/archbootstrap', __FILE__)
 include_recipe File.expand_path('../../cookbooks/rootfs_mount', __FILE__)
