0cde172080ae8b4c7f6d6285f4fea7fcd134f5fa
takumin <takumiiinn@gmail.com>
2022-06-01 00:47:25 +0900

    ignore cloud-init datasource select

--- cookbooks/cloud-init/default.rb
+++ cookbooks/cloud-init/default.rb
@@ -12,61 +12,61 @@ end
 # Constant Variables
 #
 
-DATASOURCES_AVAILABLE = [
-  'NoCloud',
-  'ConfigDrive',
-  'OpenNebula',
-  'DigitalOcean',
-  'Azure',
-  'AltCloud',
-  'OVF',
-  'MAAS',
-  'GCE',
-  'OpenStack',
-  'CloudSigma',
-  'SmartOS',
-  'Bigstep',
-  'Scaleway',
-  'AliYun',
-  'Ec2',
-  'CloudStack',
-  'Hetzner',
-  'IBMCloud',
-  'Exoscale',
-  'None',
-]
+# DATASOURCES_AVAILABLE = [
+#   'NoCloud',
+#   'ConfigDrive',
+#   'OpenNebula',
+#   'DigitalOcean',
+#   'Azure',
+#   'AltCloud',
+#   'OVF',
+#   'MAAS',
+#   'GCE',
+#   'OpenStack',
+#   'CloudSigma',
+#   'SmartOS',
+#   'Bigstep',
+#   'Scaleway',
+#   'AliYun',
+#   'Ec2',
+#   'CloudStack',
+#   'Hetzner',
+#   'IBMCloud',
+#   'Exoscale',
+#   'None',
+# ]
 
 #
 # Public Variables
 #
 
-node[:cloud_init]               ||= Hashie::Mash.new
-node[:cloud_init][:datasources] ||= ['NoCloud', 'None']
+# node[:cloud_init]               ||= Hashie::Mash.new
+# node[:cloud_init][:datasources] ||= ['NoCloud', 'None']
 
 #
 # Validate Variables
 #
 
-node.validate! do
-  {
-    cloud_init: {
-      datasources: array_of(match(/^(?:#{DATASOURCES_AVAILABLE.join('|')})$/)),
-    },
-  }
-end
+# node.validate! do
+#   {
+#     cloud_init: {
+#       datasources: array_of(match(/^(?:#{DATASOURCES_AVAILABLE.join('|')})$/)),
+#     },
+#   }
+# end
 
 #
 # Package Config
 #
 
-case node[:platform]
-when 'debian', 'ubuntu'
-  debconf 'cloud-init' do
-    question 'cloud-init/datasources'
-    vtype    'multiselect'
-    value    node[:cloud_init][:datasources].join(', ')
-  end
-end
+# case node[:platform]
+# when 'debian', 'ubuntu'
+#   debconf 'cloud-init' do
+#     question 'cloud-init/datasources'
+#     vtype    'multiselect'
+#     value    node[:cloud_init][:datasources].join(', ')
+#   end
+# end
 
 #
 # Package Install
