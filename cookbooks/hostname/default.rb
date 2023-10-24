f9656326660da9806e407ec76afd4ecaf218de91
takumin <takumiiinn@gmail.com>
2023-10-25 04:37:27 +0900

    fix dynamic hostname and hosts entry

--- cookbooks/hostname/default.rb
+++ cookbooks/hostname/default.rb
@@ -7,7 +7,7 @@
 node[:hosts]            ||= Hashie::Mash.new
 node[:hosts][:contents] ||= Hashie::Mash.new({
   '127.0.0.1': ['localhost.localdomain', 'localhost'],
-  '::1':       ['ip6-localhost', 'ip6-loopback'],
+  '::1':       ['localhost.localdomain', 'localhost', 'ip6-localhost', 'ip6-loopback'],
   'fe00::0':   ['ip6-localnet'],
   'ff00::0':   ['ip6-mcastprefix'],
   'ff02::1':   ['ip6-allnodes'],
@@ -22,7 +22,7 @@ file '/etc/hostname' do
   owner   'root'
   group   'root'
   mode    '0644'
-  content 'localhost'
+  content ''
 end
 
 #
