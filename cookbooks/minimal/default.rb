c11e73f6b333db19e85a097c4d559156c339a508
takumin <takumiiinn@gmail.com>
2023-10-28 03:28:47 +0900

    fix systemd package

--- cookbooks/minimal/default.rb
+++ cookbooks/minimal/default.rb
@@ -8,8 +8,12 @@ case node[:platform]
 when 'debian', 'ubuntu'
   # init/systemd
   package 'init'
-  package 'systemd-resolved' if node[:platform].match(/debian/)
-  package 'systemd-oomd'
+  if node[:platform].eql?('debian') and node[:platform_version].to_i >= 12
+    package 'systemd-resolved'
+    package 'systemd-oomd'
+  elsif node[:platform].eql?('ubuntu') and node[:platform_version].to_i >= 22
+    package 'systemd-oomd'
+  end
   # linux standard
   package 'lsb-release'
   # tuning
