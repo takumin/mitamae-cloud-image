d036c428c3172a494de3fe8340c5f54103d627ef
takumin <takumiiinn@gmail.com>
2023-11-17 17:41:55 +0900

    change cron time

--- cookbooks/cloud-init/default.rb
+++ cookbooks/cloud-init/default.rb
@@ -75,17 +75,6 @@ end
 package 'cloud-init'
 
 #
-# Netplan Permission
-#
-
-file '/etc/netplan/50-cloud-init.yaml' do
-  owner   'root'
-  group   'root'
-  mode    '0600'
-  only_if 'test -f /etc/netplan/50-cloud-init.yaml'
-end
-
-#
 # Package Config
 #
 
