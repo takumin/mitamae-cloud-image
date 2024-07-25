18362b45f1ab56247f81896521ff8fdd38b6d119
Takumi Takahashi <takumiiinn@gmail.com>
2024-07-26 01:44:51 +0900

    fix(liveboot): resolve Debian 12 /dev/shm error in live boot

--- cookbooks/liveboot/default.rb
+++ cookbooks/liveboot/default.rb
@@ -13,3 +13,19 @@ end
 #
 
 package 'live-boot'
+
+#
+# Workaround: Debian 12 Live Boot /dev/shm error
+# https://unix.stackexchange.com/questions/768985/debian-linux-live-system-toram-failed-to-open-dev-shm-device/774297#774297
+#
+
+if node.platform.match(/^debian$/)
+  if node.platform_version.match(/^12/)
+    file '/lib/live/boot/9990-toram-todisk.sh' do
+      action :edit
+      block do |content|
+        content.gsub!('dev="/dev/shm"', 'dev="tmpfs"')
+      end
+    end
+  end
+end
