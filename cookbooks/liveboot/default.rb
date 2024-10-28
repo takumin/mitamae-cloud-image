8d70d46fd32fdd4d3545ca8ce58781a6b3cceafd
Takumi Takahashi <takumiiinn@gmail.com>
2024-10-28 17:04:00 +0900

    refactor(liveboot): simplify workaround for /dev/shm error

--- cookbooks/liveboot/default.rb
+++ cookbooks/liveboot/default.rb
@@ -15,17 +15,13 @@ end
 package 'live-boot'
 
 #
-# Workaround: Debian 12 Live Boot /dev/shm error
+# Workaround: Systemd /dev/shm error
 # https://unix.stackexchange.com/questions/768985/debian-linux-live-system-toram-failed-to-open-dev-shm-device/774297#774297
 #
 
-if node.platform.match(/^debian$/)
-  if node.platform_version.match(/^12/)
-    file '/lib/live/boot/9990-toram-todisk.sh' do
-      action :edit
-      block do |content|
-        content.gsub!('dev="/dev/shm"', 'dev="tmpfs"')
-      end
-    end
+file '/lib/live/boot/9990-toram-todisk.sh' do
+  action :edit
+  block do |content|
+    content.gsub!('dev="/dev/shm"', 'dev="tmpfs"')
   end
 end
