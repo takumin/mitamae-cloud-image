05b28f061380e9429fce84ec9ad6b918080bca22
Takumi Takahashi <takumiiinn@gmail.com>
2024-11-08 04:51:57 +0900

    feat(kea): add service for restoring ISC Kea configuration

--- cookbooks/kea/default.rb
+++ cookbooks/kea/default.rb
@@ -67,3 +67,42 @@ end
 #
 
 package 'isc-kea'
+
+#
+# Restore Configuration
+#
+
+file '/etc/systemd/system/isc-kea-restore-config.service' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+  content <<~__EOF__
+    [Unit]
+    Description=ISC Kea Restore Configuration
+    Before=isc-kea-ctrl-agent.service
+    Before=isc-kea-dhcp-ddns-server.service
+    Before=isc-kea-dhcp4-server.service
+    Before=isc-kea-dhcp6-server.service
+    Wants=srv.mount
+    After=srv.mount
+    ConditionDirectoryNotEmpty=/srv/kea
+    ConditionPathIsDirectory=/etc/kea
+    ConditionPathIsReadWrite=/etc/kea
+
+    [Service]
+    Type=oneshot
+    ExecStart=/usr/bin/rsync -av --delete /srv/kea/ /etc/kea/
+
+    [Install]
+    WantedBy=multi-user.target
+  __EOF__
+  notifies :run, 'execute[systemctl daemon-reload]', :immediately
+end
+
+execute 'systemctl daemon-reload' do
+  action :nothing
+end
+
+service 'isc-kea-restore-config.service' do
+  action :enable
+end
